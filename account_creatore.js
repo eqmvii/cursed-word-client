3b1cc4dc3a6e55376b15b69991aab8a7073d1f34
Eric (Martin) Mancini <coelomate@gmail.com>
2022-02-22 09:00:11 -0500

    these names confused me

--- account_creatore.js
+++ account_creatore.js
@@ -1,11 +1,11 @@
 // Account Creatore
 // The extra E makes it Italian
 
-const Web3 = require('web3');
+const Web3Library = require('web3');
 
-const web3 = new Web3('ws://127.0.0.1:8545');
+const web3Connection = new Web3Library('ws://127.0.0.1:8545');
 
-const newAccount = web3.eth.accounts.create(process.argv[2] || 'pu23rple m234onkey dishw234asher zzzzz lkjafdo87243 ra942ndom');
+const newAccount = web3Connection.eth.accounts.create(process.argv[2] || 'pu23rple m234onkey dishw234asher zzzzz lkjafdo87243 ra942ndom');
 
 console.log(`\nAddress: ${newAccount.address}\nPK: ${newAccount.privateKey}\n`)
 
